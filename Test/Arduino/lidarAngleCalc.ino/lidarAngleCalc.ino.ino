2971ef83560486005ca667c409de195c76b2f64f
ThisChessPlayer <jasonma5501@gmail.com>
2017-02-08 20:53:53 -0800

    made lidarAngleCalc functionally read notches from light sensor

--- Test/Arduino/lidarAngleCalc.ino/lidarAngleCalc.ino.ino
+++ Test/Arduino/lidarAngleCalc.ino/lidarAngleCalc.ino.ino
@@ -10,25 +10,39 @@
 //TODO implement threshold before state changes can occur
 //#define STATE_CHANGE_THRESHOLD ____
 
-double times[15];   //first times of 15 notches
-double lengths[15]; //lengths of 15 notches (in units of time)
+unsigned long times[15];   //first times of 15 notches
+unsigned long lengths[15]; //lengths of 15 notches (in units of time)
 double lastAngle;   //last angle returned to ROS
 bool lastPoints[5]; //last 5 light readings for smoothing
 int currNotch;      //current notch
+int lightPin = 5;   //pin id of light sensor
 bool currState;     //state of light
 
+int debugInt;
+bool debugBool;
+
+bool debug = true;
+
  /********************************************************************
- | Routine Name: setup
+ | Routine Name: setup22
  | File:         lidarAngleCalc.ino
  | 
  | Description: Reads in 15 notches, initializes some values.
  ********************************************************************/
 void setup() {
   int i;
+  Serial.begin(9600);
+  if(debug)
+    Serial.println("Start setup");
+
+  pinMode(lightPin, INPUT); //
 
   currState = 0;
   lastAngle = 0;
   currNotch = 0;
+
+  debugInt = 0;
+  debugBool = false;
   
   recalibrateNotch();
 }
@@ -40,13 +54,15 @@ void setup() {
  | Description: Sees if recallibration is needed, then 
  ********************************************************************/
 void loop() {
+  //if(debug)
+  //  Serial.println("Start loop");
   double avgLen;
   int i, minLen, count;
 
   //calculate average length of notch as a threshold for the small notch
   for(i = 0; i < 15; i++)
     avgLen += lengths[i];
-  avgLen /= 15;
+  avgLen /= 15.0;
 
   count = 0;
   minLen = -1;
@@ -56,20 +72,43 @@ void loop() {
 
     count = 0;
     minLen = -1;
+
+    //calculate average length of notch as a threshold for the small notch
+    for(i = 0; i < 15; i++)
+      avgLen += lengths[i];
+    avgLen /= 15.0;
+  
     //if can uniquely determine one notch that is short, set that notch to be the "0" notch
     for(i = 0; i < 15; i++) {
       //finds number of lengths lower than average, if things are working properly should only be 1.
-      if(lengths[i] < avgLen) {
+      if(lengths[i] < avgLen - 4) {
         count++;
         minLen = i;
       }
     }
+    Serial.print(count);
+    Serial.print(" ");
+    /*
+    Serial.print(avgLen);
+    Serial.print('\n');
+    */
+
+    /*
+    Serial.print("Lens:\t");
+    for(i = 0; i < 15; i++) {
+      Serial.print(lengths[i]);
+      Serial.print(" ");
+    }
+    Serial.print('\n');
+    */
 
     if(count != 1) {
+      Serial.println("- Bad");
       currNotch = 0;
       recalibrateNotch();
     }
   }
+  Serial.println("+");
 
   //small notch uniquely determined
   if(minLen > 0) {
@@ -85,6 +124,23 @@ void loop() {
 
   //continue reading notches
   readNotch();
+
+  /*
+  Serial.print("Times:\t");
+  //print times and lengths
+  for(i = 0; i < 15; i++) {
+    Serial.print(times[i]);
+    Serial.print(" ");
+  }
+  Serial.print('\n');
+
+  Serial.print("Lens:\t");
+  for(i = 0; i < 15; i++) {
+    Serial.print(lengths[i]);
+    Serial.print(" ");
+  }
+  Serial.print('\n');
+  */
 }
 
  /********************************************************************
@@ -94,12 +150,14 @@ void loop() {
  | Description: Reads 15 notches
  ********************************************************************/
 void recalibrateNotch() {
+  //if(debug)
+  //  Serial.println("Start recalibrate");
   int i;
   
-  //initialize all times and lengths to -1
+  //initialize all times and lengths to 0
   for(i = 0; i < 15; i++) {
-    times[i] = -1;
-    lengths[i] = -1;
+    times[i] = 0;
+    lengths[i] = 0;
   }
   
   //fill up times and lengths array
@@ -115,21 +173,23 @@ void recalibrateNotch() {
  | Description: Reads one notch, records its time and length
  ********************************************************************/
 void readNotch() {
-  while(stateDetector() == 0) {
+  //if(debug)
+  //  Serial.println("Start readNotch");
+  while(stateDetector() == 1) {
       pollLight();
     }     
     
     //store first 1 in times[i] 
-    times[currNotch] = getTRTime();
+    times[currNotch] = millis();
     
     //check to make sure first 1 is reasonably spaced from previous times
 
     //read until 5 0s are read in a row
-    while(stateDetector() == 1) {
+    while(stateDetector() == 0) {
       pollLight();
     }
 
-    lengths[currNotch] = getTRTime() - times[currNotch];
+    lengths[currNotch] = millis() - times[currNotch];
 
     currNotch = (currNotch + 1) % 15;
 }
@@ -148,6 +208,8 @@ void readNotch() {
  | return             current state
  ********************************************************************/
 bool stateDetector() {
+  //if(debug)
+  //  Serial.println("Start stateDetector");
   int i, count;
 
   count = 0;
@@ -171,27 +233,37 @@ bool stateDetector() {
  | Description: Reads point status of light sensor and stores it in lastPoints
  ********************************************************************/
 void pollLight() {
-  //TODO read light sensor
+  //if(debug)
+  //  Serial.println("Start pollLight");
+  int i;
 
-  //TODO store read values into lastPoints
+  //shift lastPoints data
+  for(i = 0; i < 4; i++)
+    lastPoints[i] = lastPoints[i + 1];
 
-  //requires shifting lastPoints values and adding new one to end
-}
+  //read in new data
+  lastPoints[4] = digitalRead(5);
 
- /********************************************************************
- | Routine Name: getTRTime
- | File:         lidarAngleCalc.ino
- | 
- | Description: Returns current time from TeraRanger
- | 
- | Parameter Descriptions:
- | name               description
- | ------------------ -----------------------------------------------
- | return             current time from TeraRanger
- ********************************************************************/
-double getTRTime() {
-  //TODO return teraRanger time.
-  return 0;
+  /*
+  lastPoints[4] = debugBool;
+  debugInt++;
+
+  if(debugInt == 10) {
+    debugInt = 0;
+    if(debugBool)
+      debugBool = false;
+    else
+      debugBool = true;
+  }
+  */
+
+  /*
+  for(i = 0; i < 5; i++) {
+    Serial.print(lastPoints[i]);
+    Serial.print(" ");
+  }
+  Serial.print("\n");
+  */
 }
 
  /********************************************************************
@@ -206,16 +278,33 @@ double getTRTime() {
  | t                  time from TeraRanger
  | return             angle that corresponds to time
  ********************************************************************/
-double getAngle(double t) {
-  double higher, lower, intervalTime, tOnInterval;
+double getAngle(unsigned long t) {
+  //if(debug)
+  //  Serial.println("Start getAngle");
+  unsigned long higher, lower, intervalTime, tOnInterval;
   int i = 0;
   
-  while(t < times[i])
+  while(i < 15 && t >= times[i])
     i++;
 
   //TODO handle cases where i = 0, i = 14, i = 15
   //this is really up to how the times array gets updated and when old values get flushed
-    
+  
+  if(i == 0) {
+    higher = 0;
+    lower = 14;
+  }
+  else if(i == 15) {
+    higher = 0;
+    lower = 14;
+  }
+  else {
+    higher = i + 1;
+    lower = i;
+  }
+  
+  
+  
   //idea is to create interval from higher and lower times, then find where t is on that interval
   intervalTime = higher - lower;
   tOnInterval = higher - t;
@@ -234,9 +323,11 @@ double getAngle(double t) {
  | return             angle change since last call
  ********************************************************************/
 double calcAngleInterval() {
+  if(debug)
+    Serial.println("Start calcAngleInterval");
   double angleChange;
-  angleChange = getAngle(getTRTime()) - lastAngle;
-  lastAngle = getAngle(getTRTime());
+  angleChange = getAngle(millis()) - lastAngle;
+  lastAngle = getAngle(millis());
   return angleChange;
 }
 
